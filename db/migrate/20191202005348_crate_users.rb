5e85fb3d8c47b194bcced8175e72af8eef22bce8
Juan Carlo Banayo <johncban@gmail.com>
2019-12-02 00:55:18 +0000

    Automatically backed up by Learn

--- db/migrate/20191202005348_crate_users.rb
+++ db/migrate/20191202005348_crate_users.rb
@@ -1,7 +1,7 @@
 class CrateUsers < ActiveRecord::Migration[5.1]
-  def up 
+  def up
     create_table :uses do |t|
-      t.string :username 
+      t.string :username
       t.string :password_digest
     end
   end
